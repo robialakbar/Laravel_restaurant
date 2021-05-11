d1e28d91cba546d787fccda2c69302f671f1b719
robialakbar <robialakbar@gmail.com>
2021-05-11 09:22:19 +0700

    update module user

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -57,7 +57,7 @@
             <span class="nav-link">Navigation</span>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="{{user('/users')}}">
+            <a class="nav-link" href="{{url('/users')}}">
               <span class="menu-icon">
                 <i class="mdi mdi-speedometer"></i>
               </span>
